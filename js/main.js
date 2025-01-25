8c44fb4059df0d43093209a4843e95b7a0d4b40c
khalilALBader <albaderkhalil@gmail.com>
2025-01-25 15:35:27 +0300

    portfolio

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text ",{
+    strings: ["Frontrnd Developer" , "Web Developer" , "Mobile Developer","Backend Developer" ],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
